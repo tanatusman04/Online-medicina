a0fb5b7d467abe3747c6dd7a889b63133e2ce88d
tanatusman04 <tanatusman4@gmail.com>
2023-02-27 02:12:19 +0600

    baoj4

--- portal/admin.py
+++ portal/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from medicine.portal.models import Pill, Category
+
 # Register your models here.
+
+
+admin.site.register(Pill)
+
+admin.site.register(Category)
--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Pill(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    amount = models.IntegerField(verbose_name='Количество')
+    category_id = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = "Лекарства"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название', db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Категория"
